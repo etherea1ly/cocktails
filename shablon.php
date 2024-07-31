42b460261b12d1cbf6ecdabb46fff6d3ac8cf8af
etherea1ly <157814171+etherea1ly@users.noreply.github.com>
2024-07-31 16:39:52 +0500

    Add files via upload

---
+++ shablon.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <header>
+        <h1><?php echo $header; ?></h1>
+    </header>
+    <main>
+        <p><?php echo $content; ?></p>
+    </main>
+    <footer>
+        <p><?php echo $footer; ?></p>
+    </footer>
+</body>
+</html>
